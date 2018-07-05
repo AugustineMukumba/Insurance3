39f552b7354403630f220a8fbc987a4415a909de
sv-dev1 <dhiraj.dev@kindlebit.com>
2018-07-05 10:54:25 +0530

    Rest

--- InsuranceClaim/Controllers/PaypalController.cs
+++ InsuranceClaim/Controllers/PaypalController.cs
@@ -492,7 +492,7 @@ namespace InsuranceClaim.Controllers
         }
 
         [HttpPost]
-        public ActionResult SaveDeleverLicence(bool IsCheck, int Id)
+        public ActionResult SaveDeleverLicence(bool IsCheck,int Id)
         {
             try
             {
@@ -503,9 +503,10 @@ namespace InsuranceClaim.Controllers
             }
             catch (Exception)
             {
-                return Json(false, JsonRequestBehavior.AllowGet);
-            }
 
+                throw;
+            }
+     
         }
     }
 }
